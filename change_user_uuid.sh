f6132f47b1520efc5d6d36e809faf89948933116
aston-r <razor.zhenya@gmail.com>
2020-02-04 22:12:28 +0200

    add script to change the user id

---
+++ change_user_uuid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fail on any '$? > 0'
+set -o errexit
+
+user_name="${1}"
+if [[ -n "${user_name}" ]]; then
+    if [[ "$(getent 'passwd' "${user_name}")" ]]; then
+        source_uid="$(getent 'passwd' | awk -F: -v entity_name="${user_name}"
+                                               '$1==entity_name { print $3; }')"
+        target_uid="$(getent 'passwd' | awk -F: '($3>1) && ($3<10000) && ($3>maxuid) \
+                                               { maxuid=$3; } END { print maxuid+1; }')"
+
+        echo "Changing the UID of user ${user_name} from ${source_uid} to ${target_uid}"
+    fi
+else
+    echo "User with '${user_name}' does not exist!"
+fi
+
