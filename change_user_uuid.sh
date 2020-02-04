2080a7a0d68abc6dada9f9adb1b13de291cbca27
aston-r <razor.zhenya@gmail.com>
2020-02-04 22:38:28 +0200

    add error exit

--- change_user_uuid.sh
+++ change_user_uuid.sh
@@ -6,10 +6,8 @@ set -o errexit
 user_name="${1}"
 if [[ -n "${user_name}" ]]; then
     if [[ "$(getent 'passwd' "${user_name}")" ]]; then
-        source_uid="$(getent 'passwd' | awk -F: -v entity_name="${user_name}"
-                                               '$1==entity_name { print $3; }')"
-        target_uid="$(getent 'passwd' | awk -F: '($3>1) && ($3<10000) && ($3>maxuid) \
-                                               { maxuid=$3; } END { print maxuid+1; }')"
+        source_uid="$(getent 'passwd' | awk -F: -v entity_name="${user_name}" "\$1==entity_name { print \$3; }")"
+        target_uid="$(getent 'passwd' | awk -F: '($3>1) && ($3<10000) && ($3>maxuid) { maxuid=$3; } END { print maxuid+1; }')"
 
         echo "Changing the UID of user ${user_name} from ${source_uid} to ${target_uid}"
     fi
